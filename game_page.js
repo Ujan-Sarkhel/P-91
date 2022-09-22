b6bb4350c60abffc1e44a56c1b2218ba3c5cce7e
Ujan-Sarkhel <102233339+Ujan-Sarkhel@users.noreply.github.com>
2022-09-22 18:50:52 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -6,7 +6,7 @@ function send()
 
     qusetion_number = "<h4>"+number1+" X "+ number2 + "</h4>"
     input_box="<br>Answer : <input type='text' id='input_check_box'>"
-    chk_btn="<br><br><button class='btn btn-info' onclick='check()>Check</button>"
+    chk_btn="<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
     row = qusetion_number+input_box+chk_btn
     document.getElementById("output").innerHTML=row
     document.getElementById("number1").innerHTML= " "
